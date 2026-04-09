8b161edbec5968ce2b528f0bcb1261b9c9e64177
David R Git <perfectbrake1@gmail.com>
2026-04-09 14:38:32 -0500

    agregar función bajarLimones

--- limones.js
+++ limones.js
@@ -5,11 +5,19 @@ const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
+const ANCHO_LIMON=20;
+const ALTURA_LIMON=20;
+
+
 let personajeX=canvas.width/2;
+
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciarJuego(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimones();
 }
 
 function dibujarSuelo(){
@@ -40,8 +48,19 @@ function actualizarPantalla(){
     limpiarCanvas();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimones();
 }
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarLimones(){
+    ctx.fillStyle="orange";
+    ctx.fillRect(limonX, limonY, ANCHO_LIMON, ALTURA_LIMON);    
+}
+
+function bajarLimones(){
+    limonY+=10;
+    actualizarPantalla();
 }
